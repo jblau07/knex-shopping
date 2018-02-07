bddd3b28f18fc67926dd26042085e9e731136e35
jblau07 <jlau7248@gmail.com>
2018-02-06 16:22:34 -1000

    initial setup and migrations finished

---
+++ knexfile.js
@@ -0,0 +1,54 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'shopping_user',
+      password : 'password',
+      database : 'shopping_database',
+      charset: 'utf8'
+    },
+    migrations: {
+      directory: __dirname + '/knex/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/knex/seeds'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
---
+++ server.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+const knex = require('./knex/knex.js');
+
+const PORT = process.env.PORT || 3000;
+const bodyParse = require('body-parser');
+
+const cart = require('./routes/cart');
+const products = require('./routes/products');
+const users = require('./routes/users');
+
+const app = express();
+
+// app.get('/tasks', (req, res) => {
+  // use the knex variable above to create dynamic queries
+// });
+app.use('/cart', cart);
+app.use('/products', products);
+app.use('/users', users);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
